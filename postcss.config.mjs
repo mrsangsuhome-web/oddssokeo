dbfdfa9d61da33408e86c95effede99600a31ac2
Tra <mrsangsuhome@gmail.com>
2026-05-18 11:55:47 +0700

    final production build

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,11 @@
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
 
-export default config;
+  plugins: {
+
+    "@tailwindcss/postcss": {},
+
+  },
+
+}
+
+export default config
